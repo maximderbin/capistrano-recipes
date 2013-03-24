7b1d10295120f6405ccc1a5442e33498a9e223c5
babybeasimple <babybeasimple@gmail.com>
2013-03-24 13:34:45 +0400

    base refactoring

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -1,37 +1,42 @@
+# Wrapper method for quickly loading, rendering ERB templates
+# and uploading them to the server.
 def template(from, to)
   erb = File.read(File.expand_path("../templates/#{from}", __FILE__))
   put ERB.new(erb).result(binding), to
 end
 
+# Wrapper method to set default values for recipes.
 def set_default(name, *args, &block)
   set(name, *args, &block) unless exists?(name)
 end
 
-namespace :deploy do
-  desc "Install everything onto the server"
-  task :install do
-    logger.info "Install everything onto the server"
-  end
-end
-after "deploy:install", "deploy:install_dependencies"
-after "deploy:install", "deploy:install_ruby"
-after "deploy:install", "deploy:install_apps"
-after "deploy:install", "deploy:install_tools"
+# List of ruby dependencies that should be installed.
+set_default :ruby_dependencies, <<-EOS
+  build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev \
+  libcurl4-openssl-dev curl git-core
+EOS
 
+# List of common utils that should be installed.
+set_default :general_packages, <<-EOS
+  vim imagemagick mc zsh
+EOS
+
+# Will install essential / common packages.
 namespace :deploy do
-  desc "Install tools and ruby dependencies"
-  task :install_dependencies do
-    run "#{sudo} apt-get update"
-    run "#{sudo} apt-get -y install build-essential"
-    run "#{sudo} apt-get -y install zlib1g-dev"
-    run "#{sudo} apt-get -y install libssl-dev"
-    run "#{sudo} apt-get -y install libreadline-dev"
-    run "#{sudo} apt-get -y install libyaml-dev"
-    run "#{sudo} apt-get -y install libcurl4-openssl-dev"
-    run "#{sudo} apt-get -y install curl"
-    run "#{sudo} apt-get -y install git-core"
+  desc <<-EOS
+    * Updates package list
+    * Enable PPA (Custom Repositories)
+    * Installs common packages
+    * Installs Ruby dependencies
+  EOS
+  task :install do
+    run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install python-software-properties"
+    run "#{sudo} apt-add-repository ppa:blueyed/ppa"
+    run "#{sudo} apt-get -y update"
+    run "#{sudo} apt-get -y install #{general_packages} #{ruby_dependencies}"
   end
+
   desc "Download, compile and install ruby and bundler"
   task :install_ruby do
     run "wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.gz"
@@ -40,18 +45,5 @@ namespace :deploy do
     run "echo 'gem: --no-ri --no-rdoc' >> ~/.gemrc"
     run "#{sudo} gem install bundler"
   end
-  desc "Install apps enviroment dependencies"
-  task :install_apps do
-    run "#{sudo} apt-get -y install nodejs"
-    run "#{sudo} apt-get -y install imagemagick"
-    run "#{sudo} apt-get -y install mongodb"
-  end
-  desc "Install tools"
-  task :install_tools do
-    run "#{sudo} apt-get -y install vim"
-    run "#{sudo} apt-get -y install mc"
-    run "#{sudo} apt-get -y install zsh"
-    run "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
-    run "curl -L https://github.com/babybeasimple/dotfiles/raw/master/tools/install.sh | sh"
-  end
+  after "deploy:install", "deploy:install_ruby"
 end
