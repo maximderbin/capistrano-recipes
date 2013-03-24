8755f055a626ab21f4028f222053a83bbb28104f
babybeasimple <babybeasimple@gmail.com>
2013-03-24 14:11:52 +0400

    mongoDB tasks

--- config/recipes/mongodb.rb
+++ config/recipes/mongodb.rb
@@ -1,9 +1,47 @@
-# :TODO: make mongodb tasks
+# The installation adds a mongodb.conf to /etc/mongodb.conf.
+# Use this file to further configure the MongoDB server. After you finish configuring MongoDB,
+# you can either use Capistrano to restart the MongoDB server with `cap mongodb:restart`
+# or on the server through SSH with `service mongodb restart`.
+
+set_default(:mongodb_password) { Capistrano::CLI.password_prompt "MongoDB Password: " }
+
+namespace :mongodb do
+  after "deploy:install", "mongodb:install"
+  desc "Install the latest stable release of Redis."
+  task :install, roles: :db, only: {primary: true} do
+    run "#{sudo} add-apt-repository 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen'"
+    run "#{sudo} apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+    run "#{sudo} apt-get -y update"
+    run "#{sudo} apt-get -y install mongodb-10gen"
+  end
+
+  after "deploy:setup", "mongodb:setup"
+  desc "Generate the mongoid.yml configuration file."
+  task :setup, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    template "mongoid.yml.erb", "#{shared_path}/config/mongoid.yml"
+  end
+
+  after "deploy:finalize_update", "mongodb:symlink"
+  desc "Symlink the mongoid.yml file into latest release"
+  task :symlink, roles: :app do
+    run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
+  end
+
+  %w[start stop restart].each do |command|
+    desc "#{command.capitalize} MongoDB server."
+    task command do
+      run "#{sudo} service mongodb #{command}"
+    end
+  end
+end
+
+# Removes default migration tasks
 namespace :deploy do
-  task :migrate, :roles => :db  do
+  task :migrate, roles: :db  do
     logger.info "Skipping migration because mongoDB don't need it"
   end
-  task :migrations, :roles => :db  do
+  task :migrations, roles: :db  do
     logger.info "Skipping migration because mongoDB don't need it"
   end
 end
