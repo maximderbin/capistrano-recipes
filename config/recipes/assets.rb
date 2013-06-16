1cffd9ba2e6630c7f78555aae71eb8f824fc5887
babybeasimple <babybeasimple@gmail.com>
2013-06-16 14:47:32 +0400

    assets cleanup

--- config/recipes/assets.rb
+++ config/recipes/assets.rb
@@ -1,25 +1,17 @@
 # rewrites default deploy:assets:precompile task
+# don't forget uncomment load 'deploy/assets' in your Capfile!
 # compile assets only when it needed
 # compile assets locally
-# handly compile assets on remote server, if you need it
-#
-# TODO: * make code less stinky
-#       * check local and remote branch and revision before deploy
-#       * add force compile task
-#       * add remote compile task
 set_default :files_to_check, <<-FILES
-  vendor/assets/ app/assets/ lib/assets/ Gemfile.lock
+  vendor/assets/ app/assets/ lib/assets/
 FILES
 
 namespace :deploy do
   namespace :assets do
     task :precompile, roles: :web, except: { no_release: true } do
-      begin
-        from = source.next_revision(current_revision) # <-- Fail here at first-time deploy because of current/REVISION absence
-      rescue
-        err_no = true
-      end
-      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} #{files_to_check} | wc -l").to_i > 0
+      # Fails here at first-time deploy because of current/REVISION absence
+      from = source.next_revision(current_revision) rescue nil
+      if from.nil? || capture("cd #{latest_release} && #{source.local.log(from)} #{files_to_check} | wc -l").to_i > 0
         precompile_locally
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
