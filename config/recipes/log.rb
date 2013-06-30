88fe6276b134a6875fc421f9162a27aff993b22c
babybeasimple <babybeasimple@gmail.com>
2013-06-30 23:55:05 +0400

    fix(log.rb) make application log dependend of rails env

--- config/recipes/log.rb
+++ config/recipes/log.rb
@@ -1,8 +1,8 @@
 # Conventient tasks for remotely tail'ing log files.
 namespace :log do
-  desc "Stream (tail) the application's production log."
-  task :production do
-    stream "tail -f '#{shared_path}/log/production.log'"
+  desc "Stream (tail) the application log."
+  task :app do
+    stream "tail -f '#{shared_path}/log/#{rails_env}.log'"
   end
 
   desc "Stream (tail) the Unicorn log."
